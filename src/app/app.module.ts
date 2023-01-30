fb19d880e03edf2b9590159efd8dcd1cfbf5da4f
hostilelogout <80536041+hostilelogout@users.noreply.github.com>
2023-01-30 10:58:14 +0100

    added HttpClientModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { LoginPage } from './pages/login/login.page';
@@ -16,7 +17,8 @@ import { PokemonCataloguePage } from './pages/pokemon-catalogue/pokemon-catalogu
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
