f74e1597db6b19cc5ee56ea8c9b04c36f9310236
Mads Ohmsen <Ohmsen93@hotmail.com>
2023-01-30 12:03:53 +0100

    Setup Environments folder, and pokemon api service

--- src/app/pages/pokemon-catalogue/pokemon-catalogue.page.ts
+++ src/app/pages/pokemon-catalogue/pokemon-catalogue.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { PokemonApiRequestService } from '../../pokemon-api-request.service';
 
 @Component({
   selector: 'app-pokemon-catalogue',
@@ -6,5 +7,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./pokemon-catalogue.page.css']
 })
 export class PokemonCataloguePage {
+  title = 'apidata';
+  newdata:any;
+  constructor(private _apiservice:PokemonApiRequestService){}
+
+  ngOnInit(){
+    this._apiservice.getdata().subscribe(res=>{
+      this.newdata=res
+    })
+  }
 
 }
---
+++ src/app/pokemon-api-request.service.ts
@@ -0,0 +1,19 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment } from '../environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PokemonApiRequestService {
+
+  constructor(private _http:HttpClient) { }
+
+  
+
+  getdata(){
+    const apiURL = environment.apiURL;
+
+    return this._http.get(apiURL+'pokemon/');
+  }
+}
