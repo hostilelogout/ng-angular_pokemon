50e23a93de10362821ac61b5cc70127618627bd6
hostilelogout <80536041+hostilelogout@users.noreply.github.com>
2023-01-30 09:58:45 +0100

    Project Setup
    basic setup of project

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { LoginPage } from './pages/login/login.page';
+import { PokemonCataloguePage } from './pages/pokemon-catalogue/pokemon-catalogue.page';
+import { TrainerPage } from './pages/trainer/trainer.page';
+
+const routes: Routes = [
+  {
+    path:"",
+    component:LoginPage
+  },
+  {
+    path:"trainer",
+    component:TrainerPage
+  },
+  {
+    path:"pokemon-catalogue",
+    component:PokemonCataloguePage
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+
+import { AppComponent } from './app.component';
+import { LoginPage } from './pages/login/login.page';
+import { TrainerPage } from './pages/trainer/trainer.page';
+import { PokemonCataloguePage } from './pages/pokemon-catalogue/pokemon-catalogue.page';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginPage,
+    TrainerPage,
+    PokemonCataloguePage,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
