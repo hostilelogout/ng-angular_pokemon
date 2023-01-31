a80e4a212f5006d77e0e462ddaabcc1b3122a2d5
MikeTheTurtle <mikaelnia93@gmail.com>
2023-01-31 15:15:12 +0100

    FEAT: Trainer page
    Added some turbo basic scaffolding and ideas on what the trainer page could look like when importing user data and pokemon data properly

--- src/app/pages/trainer/trainer.page.ts
+++ src/app/pages/trainer/trainer.page.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-trainer',
+  selector: 'app-profile',
   templateUrl: './trainer.page.html',
   styleUrls: ['./trainer.page.css']
 })
 export class TrainerPage {
 
+  /*get trainer(): Trainer {
+    return this.trainerService.user/trainer;
+  }
+  */
+
+  /*get caughtPokemon(): Pokemon[] {
+    if (this.trainerService.user/trainer) {
+      return this.trainerService.user/trainer.caughtPokemon;
+    }
+
+    return [];
+  }*/
+
+  constructor(
+    //private trainerService: TrainerService
+  ) { }
+
+  ngOnInit(): void {
+
+  }
 }
